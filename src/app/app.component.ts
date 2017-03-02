c9ec2e748b289e9fa550499eb14b9e6daf4c9220
Vivek Poddar <vivekimsit@gmail.com>
2017-03-02 08:22:05 +0530

    Services and classes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './product.service';
+import { Product } from './product';
+
 @Component({
   selector: 'app-component',
   template: `
   <app-search-bar></app-search-bar>
   <app-product-table></app-product-table>
   `,
-  styles: []
+  styles: [],
+  providers: [ProductService]
 })
 export class AppComponent {
-  title = 'app works!';
-  filterText = '';
-  inStockOnly = false;
+  title: string;
+  products: Product[]; 
+  filterText: string;
+  inStockOnly: boolean;
+
+  constructor(private productService: ProductService) {
+    this.title = 'Product Catalogue';
+    this.filterText = '';
+    this.inStockOnly = false;
+  }
 }
